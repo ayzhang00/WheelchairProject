801bf2b9531c0e51a5d2184615aea5aee46e56d3
thomasyoun00 <thomasyoun00@gmail.com>
2022-04-11 18:45:34 -0400

    FMOD Integration

--- Assets/Scripts/XRCharacterController.cs
+++ Assets/Scripts/XRCharacterController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
+using FMODUnity;
 
 public class XRCharacterController : MonoBehaviour
 {
@@ -29,6 +30,13 @@ public class XRCharacterController : MonoBehaviour
     private float moveR;
     private float moveL;
 
+    //Character speed
+    private float maxSpeed = 1.0f;
+
+    //Fmod reference (for audio)
+    FMOD.Studio.EventInstance wheels;
+    [FMODUnity.EventRef]
+    FMOD.Studio.PARAMETER_ID wheelSpeedID;
 
   // Called at the very beginning, before start (not necessary but useful)
     private void Awake()
@@ -67,6 +75,19 @@ public class XRCharacterController : MonoBehaviour
             localZAxis_L = posL.z;
         }
 
+        // Create FMOD instance & attach to game object
+        wheels = FMODUnity.RuntimeManager.CreateInstance("event:/Wheels");
+
+        FMOD.Studio.EventDescription wheelsEventDescription;
+        wheels.getDescription(out wheelsEventDescription);
+        FMOD.Studio.PARAMETER_DESCRIPTION wheelsParameterDescription;
+        wheelsEventDescription.getParameterDescriptionByName("WheelSpeed", out wheelsParameterDescription);
+        wheelSpeedID = wheelsParameterDescription.id;
+
+        wheels.start();
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(wheels, GetComponent<Transform>(), GetComponent<Rigidbody>());
+ 
+
     }
 
 
@@ -83,6 +104,15 @@ public class XRCharacterController : MonoBehaviour
 
         UpdateWheelPoses();
 
+        //updates character speed every frame
+        Rigidbody rb = GetComponent<Rigidbody>();
+        Vector3 velocity = rb.velocity;
+
+        rb.velocity = velocity.normalized * maxSpeed;
+        Debug.Log(rb.velocity.magnitude);
+
+        //update wheelspeed for fmod
+        wheels.setParameterByID(wheelSpeedID, rb.velocity.magnitude);
     }
 
   // checks for movement, sets direction, and applies movement
